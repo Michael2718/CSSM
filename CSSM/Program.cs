65f98e4bafecda41c07198831c9dc2b496a3388b
Michael2718 <isaevmihail007@gmail.com>
2022-10-31 18:08:00 +0200

    Second commit

--- CSSM/Program.cs
+++ CSSM/Program.cs
@@ -1,15 +1,8 @@
-namespace CSSM
-{
+namespace CSSM {
     internal static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
         [STAThread]
-        static void Main()
-        {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
+        static void Main() {
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
         }
