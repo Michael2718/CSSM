fe4218bbf9bb09319c10de6742d1c052fd546652
Michael2718 <isaevmihail007@gmail.com>
2022-11-17 20:38:46 +0200

    Add Settings Fields

--- CSSM/src/Settings.cs
+++ CSSM/src/Settings.cs
@@ -1,10 +1,36 @@
 ﻿namespace CSSM {
     class Settings {
-        public double Intensity { get; set; }
-        public int MinValueOfBurstTime { get; set; }
-        public int MaxValueOfBurstTime { get; set; }
-        public int MinValueOfAddrSpace { get; set; }
-        public int MaxValueOfAddrSpace { get; set; }
-        public int ValueOfRAMSize { get; set; }
+        private double intensity;
+        private int minValueOfBurstTime;
+        private int maxValueOfBurstTime;
+        private int minValueOfAddrSpace;
+        private int maxValueOfAddrSpace;
+        private int valueOfRAMSize;
+        public double Intensity {
+            get { return intensity; }
+            set { intensity = value; }
+        }
+        public int MinValueOfBurstTime {
+            get { return minValueOfBurstTime; }
+            set { minValueOfBurstTime = value; }
+        }
+        public int MaxValueOfBurstTime {
+            get { return maxValueOfBurstTime; }
+            set { maxValueOfBurstTime = value; }
+        }
+        public int MinValueOfAddrSpace {
+            get { return minValueOfAddrSpace; }
+            set { minValueOfAddrSpace = value; }
+
+        }
+        public int MaxValueOfAddrSpace {
+            get { return maxValueOfAddrSpace; }
+            set { maxValueOfAddrSpace = value; }
+
+        }
+        public int ValueOfRAMSize {
+            get { return valueOfRAMSize; }
+            set { valueOfRAMSize = value; }
+        }
     }
 }
